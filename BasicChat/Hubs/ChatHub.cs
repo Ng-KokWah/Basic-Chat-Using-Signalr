1a26f0d970f0c43e84547a16a4106513ad7b7923
Ng Kok Wah <kokwah98@gmail.com>
2020-10-27 14:57:18 +0800

    Add files via upload

---
+++ BasicChat/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace BasicChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            DateTime dateTime = DateTime.Now;
+            string date = dateTime.ToShortDateString();
+            string time = dateTime.ToShortTimeString();
+            await Clients.All.SendAsync("ReceiveMessage", user, message, date + " " + time);
+        }
+    }
+}
